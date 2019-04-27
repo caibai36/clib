3a80262eaa6bf7a98f8ae3976345432d0f870cec
bin-wu <wu.bin.vq9@is.naist.jp>
2019-04-27 22:43:31 +0900

    Feature Extraction

--- egs/wsj/s0/run.sh
+++ egs/wsj/s0/run.sh
@@ -11,7 +11,9 @@ sh local/kaldi_conf.sh
 . path.sh
 
 # general configuration
-stage=0        # start from 0 if you need to start from data preparation
+stage=1  # start from 0 if you need to start from data preparation
+mfcc_config=conf/mfcc_hires.conf  # use the high-resolution mfcc for training the neurnal network;
+                             # 40 dimensional mfcc feature used by Google and kaldi wsj network training.
 
 # Parse the options. (eg. ./run.sh --stage 1)
 # Note that the options should be defined as shell variable before parsing
@@ -22,11 +24,38 @@ stage=0        # start from 0 if you need to start from data preparation
 # wsj1=/project/nakamura-lab01/Share/Corpora/Speech/en/WSJ/wsj1
 wsj=/project/nakamura-lab01/Share/Corpora/Speech/en/WSJ/
 
-if [ ${stage} -le 0 ]; then
+if [ $stage -le 0 ]; then
     date
-    echo "stage 0: Data preparation"
+    echo "Stage 0: Data Preparation"
     # local/wsj_data_prep.sh ${wsj0}/??-{?,??}.? ${wsj1}/??-{?,??}.?
-    local/cstr_wsj_data_prep.sh $wsj
-    local/wsj_format_data.sh
+    local/cstr_wsj_data_prep.sh $wsj || exit 1;
+    local/wsj_format_data.sh || exit 1;
+    date
+fi
+
+# Directory contains mfcc features and cmvn statistics.
+mfcc_dir=mfcc
+if [ $stage -le 1 ]; then
+    date
+    echo "Stage 1: Feature Extraction"
+    for x in test_eval92 test_eval93 test_dev93 train_si284; do
+	# Do Mel-frequency cepstral coefficients (mfcc) feature extraction.
+	# We use the 40-dim mfcc by kaldi neural network training default.
+	steps/make_mfcc.sh --cmd "$train_cmd" --nj 20 \
+			   --mfcc-config $mfcc_config --write_utt2num_frames true \
+			   data/$x exp/make_mfcc/$x $mfcc_dir || exit 1
+	# Compute Cepstral mean and variance normalization (cmvn) of data.
+	steps/compute_cmvn_stats.sh data/$x exp/make_mfcc/$x $mfcc_dir || exit 1;
+	# Fixing data format.
+	utils/fix_data_dir.sh data/$x || exit 1 # remove segments with problems  
+    done
+    
+    utils/subset_data_dir.sh --first data/train_si284 7138 data/train_si84 || exit 1
+
+    # Now make subset with the shortest 2k utterances from si-84.
+    utils/subset_data_dir.sh --shortest data/train_si84 2000 data/train_si84_2kshort || exit 1;
+
+    # Now make subset with half of the data from si-84.
+    utils/subset_data_dir.sh data/train_si84 3500 data/train_si84_half || exit 1;
     date
 fi
