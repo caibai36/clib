ea380de199b49a97cae3663fa725fe243cafc9ed
bin-wu <wu.bin.vq9@is.naist.jp>
2019-12-13 23:06:27 +0900

    initilize dataloader

---
+++ egs/wsj/s0/local/sandbox/test_asr.py
@@ -0,0 +1,42 @@
+# Add clib package at current directory to the binary searching path.
+import sys
+import os
+sys.path.append(os.getcwd())
+
+import json
+import pprint
+import argparse
+import torch
+from clib.kaldi.kaldi_data import KaldiDataLoader, KaldiDataset 
+
+# created by local/script/create_simple_utts_json.py
+json_file = 'data/test_small/utts.json'
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--json-file', type=str, default=json_file, help="the test utterance json file")
+# We follow the index convention of torchtext by setting padding id as 1.
+parser.add_argument('--padding_tokenid', type=int, default=1, help="the id of padding token")
+
+args = parser.parse_args()
+
+with open(args.json_file, encoding='utf8') as f:
+    # utts_json is a dictionary mapping utt_id to fields of each utterance
+    utts_json = json.load(f)
+    # Each utterance instance is a list of fields includes 'feat', 'tokenid' and etc.
+    utts_instances = list(utts_json.values())
+
+    dataset = KaldiDataset(utts_instances)
+    dataloader = KaldiDataLoader(dataset=dataset, batch_size=3, padding_tokenid=args.padding_tokenid)
+
+    print("the kaldi dataset")
+    for instance in dataset:
+        pprint.pprint(instance)
+
+    print("the kaldi dataloader")
+    batches = []
+    for batch in dataloader:
+        batches.append(batch)
+
+    for batch in batches:
+        pprint.pprint(batch)
+    
