25cd7862f9c997e7ee2676b4e2efa3b7e5eb6d67
bin-wu <wu.bin.vq9@is.naist.jp>
2019-12-28 19:01:02 +0900

    constructor and test example of EncRNNDecRNNAtt

--- egs/wsj/s0/local/sandbox/test_asr.py
+++ egs/wsj/s0/local/sandbox/test_asr.py
@@ -246,18 +246,8 @@ class PyramidRNNEncoder(nn.Module):
 
         self.output_size = pre_size
 
-    def get_config(self):
-        return { 'class': str(self.__class__),
-                 'enc_input_size': self.enc_input_size,
-                 'enc_fnn_sizes': self.enc_fnn_sizes,
-                 'enc_fnn_act': self.enc_fnn_act,
-                 'enc_fnn_dropout': self.enc_fnn_dropout,
-                 'enc_rnn_sizes': self.enc_rnn_sizes,
-                 'enc_rnn_config': self.enc_rnn_config,
-                 'enc_rnn_dropout': self.enc_rnn_dropout,
-                 'enc_rnn_subsampling': self.enc_rnn_subsampling,
-                 'enc_rnn_subsampling_type': self.enc_rnn_subsampling_type,
-                 'enc_input_padding_value': self.enc_input_padding_value}
+    def get_context_size(self):
+        return self.output_size
 
     def encode(self,
                input: torch.Tensor,
@@ -597,7 +587,7 @@ class LuongDecoder(nn.Module):
     def reset(self):
         self.attentional_vector_pre = None
 
-    def forward(self, input, dec_mask=None):
+    def forward(self, input: torch.Tensor, dec_mask: torch.Tensor = None) -> (torch.Tensor, Dict):
         """
         input: batch_size x input_size
         dec_mask: batch_size
@@ -635,28 +625,213 @@ class LuongDecoder(nn.Module):
 
         return output, att_out
 
+def test_luong_decoder():
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print("Device: '{}'".format(device))
+
+    input_embedding = torch.Tensor([[0.3, 0.4], [0.3, 0.5]])
+    context = torch.Tensor([[[3, 4], [4, 3]], [[3, 5], [3, 4]]])
+    context_mask = torch.ByteTensor([[1, 1],[1, 0]])
+
+    luong_decoder = LuongDecoder(att_config={"type": "mlp"},
+                                 context_size=context.shape[-1],
+                                 input_size=input_embedding.shape[-1],
+                                 rnn_sizes=[512, 512],
+                                 rnn_config={"type": "lstmcell"},
+                                 rnn_dropout=0.25,
+                                 context_proj_size=3, # the size of attentional vector
+                                 context_proj_act='tanh')
+    input_embedding, context, context_mask = input_embedding.to(device), context.to(device), context_mask.to(device)
+    luong_decoder.to(device)
+    luong_decoder.set_context(context, context_mask)
+    output, att_out = luong_decoder(input_embedding)
+    # output, att_out = luong_decoder(input_embedding, dec_mask=torch.Tensor([0, 1])) # mask the first instance in two batches
+    print("output of Luong decoder: {}".format(output))
+    print("output of attention layer: {}".format(att_out))
+
+class EncRNNDecRNNAtt(nn.Module):
+    """ Sequence-to-sequence module with RNN encoder and RNN decoder with attention mechanism.
+
+    The default encoder is pyramid RNN Encoder (https://arxiv.org/abs/1508.01211 "LAS" section 3.1 formula (5)),
+    which passes the input feature through forward feedback neural network ('enc_fnn_*') and RNN ('enc_rnn_*').
+    Between the RNN layers we use the subsampling ('enc_rnn_subsampling_*') by concatenating both frames or taking the first frame every two frames.
+
+    The default decoder is Luong Decoder (https://arxiv.org/abs/1508.04025 "Effective MNT"
+    section 3 formula (5) to create attentional vector; section 3.3 the input feeding approach)
+    which passes the embedding of the input ('dec_embedding_*') along with previous attentional vector into a stacked RNN layers ('dec_rnn_*'),
+    linearly projects the top RNN hidden state and expected context vector to the current attentional vector ('dec_context_proj_*'),
+    and feed the attentional vector to next time step.
+
+    The default attention is the multilayer perception attention by concatenation (https://arxiv.org/abs/1508.04025 "Effective MNT" section 3.1 formula (8) (concat version))
+    which concatenates the hidden state from decoder and each part of context from encoder and passes them to two layers neural network to get the alignment score.
+    The scores are then normalized to get the probability of different part of context and get the expected context vector.
+
+    We tie the embedding weight by default (https://arxiv.org/abs/1608.05859 "Using the Output Embedding to Improve Language Models" introduction),
+    which shares the weights between (dec_onehot => dec_embedding) and (output_embedding(attentional vector) => pre_softmax)
+
+    Information flow:
+    encode:
+    enc_input->encoder->context
+
+    decode with one time step:
+    dec_input->dec_embedding->dec_hidden
+    (context,dec_hidden)->context_vector
+    (context_vector,dec_hidden)->attentional_vector
+    attentional_vector->pre_softmax
+    """
+    def __init__(self,
+                 enc_input_size: int,
+                 dec_input_size: int,
+                 dec_output_size: int,
+                 enc_fnn_sizes: List[int] = [512],
+                 enc_fnn_act: str = 'relu',
+                 enc_fnn_dropout: Union[float, List[float]] = 0.25,
+                 enc_rnn_sizes: List[int] = [256, 256, 256],
+                 enc_rnn_config: Dict = {'type': 'lstm', 'bi': True},
+                 enc_rnn_dropout: Union[float, List[float]] = 0.25,
+                 enc_rnn_subsampling: Union[bool, List[bool]] = [False, True, True],
+                 enc_rnn_subsampling_type: str = 'pair_concat', # 'pair_concat' or 'pair_take_first'
+                 dec_embedding_size: int = 256,
+                 dec_embedding_dropout: float = 0.25,
+                 dec_embedding_weights_tied: bool = True, # share weights between (input_onehot => input_embedding) and (output_embedding => pre_softmax)
+                 dec_rnn_sizes: List[int] = [512, 512],
+                 dec_rnn_config: Dict = {"type": "lstmcell"},
+                 dec_rnn_dropout: Union[List[float], float] = 0.25,
+                 dec_context_proj_size: int = 256, # the size of attentional vector
+                 dec_context_proj_act: str = 'tanh',
+                 dec_context_proj_dropout: int = 0.25,
+                 enc_config: Dict = {'type': 'pyramid_rnn_encoder'},
+                 dec_config: Dict = {'type': 'luong_decoder'},
+                 att_config: Dict = {'type': 'mlp'}, # configuration of attention
+    ):
+        super().__init__()
+
+        self.enc_input_size = enc_input_size
+        self.dec_input_size = dec_input_size
+        self.dec_output_size = dec_output_size
+        self.enc_fnn_sizes = enc_fnn_sizes
+        self.enc_fnn_act = enc_fnn_act
+        self.enc_fnn_dropout = enc_fnn_dropout
+        self.enc_rnn_sizes = enc_rnn_sizes
+        self.enc_rnn_config = enc_rnn_config
+        self.enc_rnn_dropout = enc_rnn_dropout
+        self.enc_rnn_subsampling = enc_rnn_subsampling
+        self.enc_rnn_subsampling_type = enc_rnn_subsampling_type
+        self.dec_embedding_size = dec_embedding_size
+        self.dec_embedding_dropout = dec_embedding_dropout
+        self.dec_embedding_weights_tied = dec_embedding_weights_tied
+        self.dec_rnn_sizes = dec_rnn_sizes
+        self.dec_rnn_config = dec_rnn_config
+        self.dec_rnn_dropout = dec_rnn_dropout
+        self.dec_context_proj_size = dec_context_proj_size
+        self.dec_context_proj_act = dec_context_proj_act
+        self.dec_context_proj_dropout = dec_context_proj_dropout
+        self.enc_config = enc_config
+        self.dec_config = dec_config
+        self.att_config = att_config
+
+        assert enc_config['type'] == 'pyramid_rnn_encoder', \
+            "The encoder type '{}' is not implemented. Supported types include 'pyramid_rnn_encoder'.".format(enc_config['type'])
+        assert dec_config['type'] == 'luong_decoder', \
+            "The decoder type '{}' is not implemented. Supported types include 'luong_encoder'.".format(dec_config['type'])
+
+        # Encoder
+        self.encoder = PyramidRNNEncoder(enc_input_size=enc_input_size,
+                                         enc_fnn_sizes=enc_fnn_sizes,
+                                         enc_fnn_act=enc_fnn_act,
+                                         enc_fnn_dropout=enc_fnn_dropout,
+                                         enc_rnn_sizes=enc_rnn_sizes,
+                                         enc_rnn_config=enc_rnn_config,
+                                         enc_rnn_dropout=enc_rnn_dropout,
+                                         enc_rnn_subsampling=enc_rnn_subsampling,
+                                         enc_rnn_subsampling_type=enc_rnn_subsampling_type,
+                                         enc_input_padding_value=0.0)
+        self.enc_context_size = self.encoder.get_context_size()
+
+        # Embedder
+        self.dec_embedder = nn.Embedding(dec_input_size, dec_embedding_size, padding_idx=None)
+
+        # Decoder
+        self.decoder = LuongDecoder(att_config=att_config,
+                                    context_size=self.enc_context_size,
+                                    input_size=dec_embedding_size,
+                                    rnn_sizes=dec_rnn_sizes,
+                                    rnn_config=dec_rnn_config,
+                                    rnn_dropout=dec_rnn_dropout,
+                                    context_proj_size=dec_context_proj_size,
+                                    context_proj_act=dec_context_proj_act,
+                                    context_proj_dropout=dec_context_proj_dropout)
+
+        # Presoftmax
+        self.dec_presoftmax = nn.Linear(dec_context_proj_size, dec_output_size) # decoder.output_size == dec_context_proj_size
+
+        # Typing weight
+        if (dec_embedding_weights_tied):
+            assert (dec_input_size, dec_embedding_size) == (dec_output_size, dec_context_proj_size), \
+                f"When tying embedding weights: the shape of embedder weights: (dec_input_size = {dec_input_size}, dec_embedding_size = {dec_embedding_size})\n" + \
+                f"should be same as the shape of presoftmax weights: (dec_output_size = {dec_output_size}, dec_context_proj_size = {dec_context_proj_size})"
+            # tie weights between dec_embedder(input_onehot => input_embedding) and presoftmax(output_embedding => pre_softmax)
+            self.dec_presoftmax.weight = self.dec_embedder.weight
+
+    def get_config(self):
+        return { 'class': str(self.__class__),
+                 'enc_input_size': self.enc_input_size,
+                 'dec_input_size': self.dec_input_size,
+                 'dec_output_size': self.dec_output_size,
+                 'enc_fnn_sizes': self.enc_fnn_sizes,
+                 'enc_fnn_act': self.enc_fnn_act,
+                 'enc_fnn_dropout': self.enc_fnn_dropout,
+                 'enc_rnn_sizes': self.enc_rnn_sizes,
+                 'enc_rnn_config': self.enc_rnn_config,
+                 'enc_rnn_dropout': self.enc_rnn_dropout,
+                 'enc_rnn_subsampling': self.enc_rnn_subsampling,
+                 'enc_rnn_subsampling_type': self.enc_rnn_subsampling_type,
+                 'dec_embedding_size': self.dec_embedding_size,
+                 'dec_embedding_dropout': self.dec_embedding_dropout,
+                 'dec_embedding_weights_tied': self.dec_embedding_weights_tied,
+                 'dec_rnn_sizes': self.dec_rnn_sizes,
+                 'dec_rnn_config': self.dec_rnn_config,
+                 'dec_rnn_dropout': self.dec_rnn_dropout,
+                 'dec_context_proj_size': self.dec_context_proj_size,
+                 'dec_context_proj_act': self.dec_context_proj_act,
+                 'dec_context_proj_dropout': self.dec_context_proj_dropout,
+                 'enc_config': self.enc_config,
+                 'dec_config': self.dec_config,
+                 'att_config': self.att_config}
+
 # test_encoder()
 # test_attention()
+# test_luong_decoder()
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-print("Device: '{}'".format(device))
+batch = next(iter(dataloader))
+# batch = batches[1]
+enc_input = batch['feat']
+enc_input_lengths = batch['num_frames'] # or None
+enc_input_size = enc_input.shape[-1]
+dec_input = batch['tokenid']
+dec_input_size = batch['vocab_size']
+dec_output_size = batch['vocab_size']
 
-input_embedding = torch.Tensor([[0.3, 0.4], [0.3, 0.5]])
-context = torch.Tensor([[[3, 4], [4, 3]], [[3, 5], [3, 4]]])
-context_mask = torch.ByteTensor([[1, 1],[1, 0]])
-
-luong_decoder = LuongDecoder(att_config={"type": "mlp"},
-                             context_size=context.shape[-1],
-                             input_size=input_embedding.shape[-1],
-                             rnn_sizes=[512, 512],
-                             rnn_config={"type": "lstmcell"},
-                             rnn_dropout=0.25,
-                             context_proj_size=3, # the size of attentional vector
-                             context_proj_act='tanh')
-input_embedding, context, context_mask = input_embedding.to(device), context.to(device), context_mask.to(device)
-luong_decoder.to(device)
-luong_decoder.set_context(context, context_mask)
-output, att_out = luong_decoder(input_embedding)
-# output, att_out = luong_decoder(input_embedding, dec_mask=torch.Tensor([0, 1])) # mask the first instance in two batches
-print("output of Luong decoder: {}".format(output))
-print("output of attention layer: {}".format(att_out))
+model = EncRNNDecRNNAtt(enc_input_size,
+                        dec_input_size,
+                        dec_output_size,
+                        enc_fnn_sizes=[4, 9],
+                        enc_fnn_act='relu',
+                        enc_fnn_dropout=0.25,
+                        enc_rnn_sizes=[5, 5, 5],
+                        enc_rnn_config={'type': 'lstm', 'bi': True},
+                        enc_rnn_dropout=0.25,
+                        enc_rnn_subsampling=[False, True, True],
+                        enc_rnn_subsampling_type='pair_concat',
+                        dec_embedding_size=6,
+                        dec_embedding_dropout=0.25,
+                        dec_embedding_weights_tied=True,
+                        dec_rnn_sizes=[8, 8],
+                        dec_rnn_config={"type": "lstmcell"},
+                        dec_rnn_dropout=0.25,
+                        dec_context_proj_size=6,
+                        dec_context_proj_act='tanh',
+                        dec_context_proj_dropout=0.25,
+                        enc_config={'type': 'pyramid_rnn_encoder'},
+                        dec_config={'type': 'luong_decoder'},
+                        att_config={'type': 'mlp'})
