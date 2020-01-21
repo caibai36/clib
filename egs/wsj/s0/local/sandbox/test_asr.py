ee83c9bb0692dd03566c242f8fc6a6c67da6a15f
bin-wu <wu.bin.vq9@is.naist.jp>
2020-01-22 00:08:42 +0900

    add mask2length method

--- egs/wsj/s0/local/sandbox/test_asr.py
+++ egs/wsj/s0/local/sandbox/test_asr.py
@@ -144,27 +144,28 @@ def length2mask(sequence_lengths: torch.Tensor, max_length: Union[int, None] = N
     our input was ``[2, 2, 3]``, with a ``max_length`` of 4, we'd return
     ``[[1, 1, 0, 0], [1, 1, 0, 0], [1, 1, 1, 0]]``.
 
-    Examples:
-        sequence_lengths: [2, 2, 3], max_length: 4: -> mask: [[1, 1, 0, 0], [1, 1, 0, 0], [1, 1, 1, 0]]
+    Examples
+    --------
+    sequence_lengths: [2, 2, 3], max_length: 4: -> mask: [[1, 1, 0, 0], [1, 1, 0, 0], [1, 1, 1, 0]]
 
-        In [451]: lengths = torch.tensor([2, 2, 3])
-        In [452]: length2mask(lengths, 4)
-        Out[452]:
-        tensor([[1, 1, 0, 0],
-                [1, 1, 0, 0],
-                [1, 1, 1, 0]])
+    In [451]: lengths = torch.tensor([2, 2, 3])
+    In [452]: length2mask(lengths, 4)
+    Out[452]:
+    tensor([[1, 1, 0, 0],
+            [1, 1, 0, 0],
+            [1, 1, 1, 0]])
 
-        In [453]: length2mask(lengths, 2)
-        Out[453]:
-        tensor([[1, 1],
-                [1, 1],
-                [1, 1]])
+    In [453]: length2mask(lengths, 2)
+    Out[453]:
+    tensor([[1, 1],
+            [1, 1],
+            [1, 1]])
 
-        In [276]: length2mask(lengths)
-        Out[276]:
-        tensor([[1, 1, 0],
-                [1, 1, 0],
-                [1, 1, 1]])
+    In [276]: length2mask(lengths)
+    Out[276]:
+    tensor([[1, 1, 0],
+            [1, 1, 0],
+            [1, 1, 1]])
     """
     if max_length is None:
         max_length = int(torch.max(sequence_lengths).item())
@@ -172,6 +173,31 @@ def length2mask(sequence_lengths: torch.Tensor, max_length: Union[int, None] = N
     cumsum_ones = ones_seqs.cumsum(dim=-1)
 
     return (cumsum_ones <= sequence_lengths.unsqueeze(-1)).long()
+
+def mask2length(mask: torch.Tensor) -> torch.LongTensor:
+    """
+    Compute sequence lengths for the batch from a binary mask.
+
+    Parameters
+    ----------
+    mask: a binary mask of shape [batch_size, sequence_length]
+
+    Returns
+    -------
+    the lengths of the sequences in the batch of shape [batch_size]
+
+    Example
+    -------
+    In [458]: mask
+    Out[458]:
+    tensor([[1, 1, 0, 0],
+            [1, 1, 0, 0],
+            [1, 1, 1, 0]])
+
+    In [459]: mask2length(mask)
+    Out[459]: tensor([2, 2, 3])
+    """
+    return mask.long().sum(-1)
 
 class CrossEntropyLossLabelSmoothing(nn.Module):
     """ Cross entropy loss function support label smoothing and weight of classes.
@@ -202,18 +228,18 @@ class CrossEntropyLossLabelSmoothing(nn.Module):
     Example
     -------
     Input:
-        source = torch.Tensor([[0.9, 0.2, 0.3], [0.1, 0.9, 0.3], [0.9, 0.2, 0.3]])
-        target = torch.LongTensor([1, 2, 1])
+    source = torch.Tensor([[0.9, 0.2, 0.3], [0.1, 0.9, 0.3], [0.9, 0.2, 0.3]])
+    target = torch.LongTensor([1, 2, 1])
 
-        label_smoothing = 0.8
-        weight = torch.Tensor([0.1, 0.5, 0.4])
-        reduction = 'none'
+    label_smoothing = 0.8
+    weight = torch.Tensor([0.1, 0.5, 0.4])
+    reduction = 'none'
 
-        ce_loss = CrossEntropyLossLabelSmoothing(label_smoothing=label_smoothing, weight=weight, reduction=reduction)
-        print(ce_loss(source, target))
+    ce_loss = CrossEntropyLossLabelSmoothing(label_smoothing=label_smoothing, weight=weight, reduction=reduction)
+    print(ce_loss(source, target))
 
     Output:
-        tensor([0.6011, 0.4742, 0.6011])
+    tensor([0.6011, 0.4742, 0.6011])
     """
     def __init__(self,
                  label_smoothing: float = 0.,
