61dc047a2c38cc29b21743c32aee8aca19ada510
bin-wu <wu.bin.vq9@is.naist.jp>
2020-04-02 17:55:09 +0900

    merge function of replace_str.py to text2token.py

--- egs/wsj/s0/local/script/data2json.sh
+++ egs/wsj/s0/local/script/data2json.sh
@@ -2,7 +2,7 @@
 
 # Copyright 2017 Johns Hopkins University (Shinji Watanabe)
 #  Apache 2.0  (http://www.apache.org/licenses/LICENSE-2.0)
-# Last modified at 18:16 on 06 December 2019 by WuBin. 
+# Last modified at 18:16 on 06 December 2019 by WuBin.
 . ./path.sh
 
 # Get the num_frames.scp, feat_dim.scp, num_tokens.scp, tokenid.scp, vocab_size.scp, feat.scp, token.scp and etc.
@@ -37,17 +37,21 @@ if [ ! -z ${feat} ]; then
     feat-to-dim scp:${feat} ark,t:${tmpdir}/feat_dim.scp 1>&2
 fi
 
-# Get token.scp, tokenid.scp, num_tokens.scp and vocab_size.scp 
+# Get token.scp, tokenid.scp, num_tokens.scp and vocab_size.scp
 # Add <sos> and <eos> at the beginning and ending of the sentences (token.scp)
 # Count how many tokens for each sentence (num_tokens.scp)
 # Convert token sequence to tokenid sequence (tokenid.scp)
-cat ${dir}/text | tr [A-Z] [a-z] | \
-    python local/script/replace_str.py --rep_in=conf/str_rep.txt --sep='#' | \
-    local/script/text2token.py --skip-ncols=1 --non-ling-syms=${non_ling_syms} --chars-delete=conf/chars_del.txt --chars-replace=conf/chars_rep.txt | \
-    tr [A-Z] [a-z] | sed -r -e 's/^(\w*) /\1 <sos> /' -e 's/$/ <eos>/' > ${tmpdir}/token.scp
+local/script/text2token.py --text ${dir}/text \
+			   --strs-replace-in=conf/str_rep.txt \
+			   --strs-replace-sep='#' \
+			   --chars-delete=conf/chars_del.txt \
+			   --chars-replace=conf/chars_rep.txt \
+			   --non-ling-syms=${non_ling_syms} \
+			   --skip-ncols=1 \
+			   --str2lower | sed -r -e 's/^(\w*) /\1 <sos> /' -e 's/$/ <eos>/' > ${tmpdir}/token.scp
 cat ${tmpdir}/token.scp | utils/sym2int.pl --map-oov ${oov} -f 2- ${dict} > ${tmpdir}/tokenid.scp
 cat ${tmpdir}/tokenid.scp | awk '{print $1 " " NF-1}' > ${tmpdir}/num_tokens.scp # -1 for uncounting first field: the uttid
-vocsize=`tail -n 1 ${dict} | awk '{print $2}'` # Get the index of the last word, assuming the largest index starting from 0 (the $vocab_size -1 ). 
+vocsize=`tail -n 1 ${dict} | awk '{print $2}'` # Get the index of the last word, assuming the largest index starting from 0 (vocsize=$vocab_size-1).
 odim=`echo "$vocsize + 1" | bc`
 awk -v odim=${odim} '{print $1 " " odim}' ${dir}/text > ${tmpdir}/vocab_size.scp
 
--- egs/wsj/s0/local/script/text2token.py
+++ egs/wsj/s0/local/script/text2token.py
@@ -29,7 +29,7 @@ Example:
     T t
     O o
     ABOUT <UNK>
-    (base) [bin-wu@ahccsclm03 s0]$ cutils/text2token.py -n 2 -s 2 -l $non_ling_sym -d $chars_del -r $chars_rep $text 
+    (base) [bin-wu@ahccsclm03 s0]$ cutils/text2token.py -n 2 -s 2 -l $non_ling_sym -d $chars_del -r $chars_rep $text
 4k0c0201 THE SE EN <space>U NI S<space> o<space> BE <space>o FF ER ED <space>F oR <space>S AL E<space> HA E<space> A<space> Wo RK <space>F oR CE <space>o F<space> <UNK><space> WE NY <space>H oU SAND
 4k0c0202 AND AL LI ED <space>S IG NA L<space> EM PL oY S<space> A<space> oA L<space> oF <space><UNK> <space>o NE <space>H UN DR ED <space>AND <space>F oR Y<space> Ho US AND<space> Wo RK ER S
 '''
@@ -51,7 +51,7 @@ def is_non_ling_syms_index(matched_pos: List[Pos], cur_pos: int) -> Optional[Pos
 def main():
     parser = argparse.ArgumentParser(
         formatter_class=argparse.RawDescriptionHelpFormatter, epilog=example_string)
-    parser.add_argument('text', type=str, default=False, nargs='?',
+    parser.add_argument('--text', type=str, default=False, nargs='?',
                         help='input text')
     parser.add_argument('--non-ling-syms', '-l', default=None, type=str,
                         help='list of non-linguistic symbols with format <XXX>, e.g., <NOISE> etc.')
@@ -68,7 +68,14 @@ def main():
                         help="file contains character pairs to be replaced at text. eg. O o or <*IN*> <UNK>")
     parser.add_argument('--chars-replace-sep', default=" ", type=str,
                         help="separator of the character pairs in the chars-replace file")
-    
+    parser.add_argument("--str2lower", action='store_true', default=False,
+                        help="convert the characters of string to lower case before and after all other operations")
+    parser.add_argument("--strs-replace-in", type=str, default=None,
+                        help="the string replacement file, default separator is ':'; first replace strs, then delete and replace chars")
+    parser.add_argument("--strs-replace-sep", type=str, default=":",
+                        help="the separator of the string replacement file")
+
+
     args = parser.parse_args()
 
     if args.text:
@@ -76,6 +83,13 @@ def main():
     else:
         # f = sys.stdin
         f = io.TextIOWrapper(sys.stdin.buffer, encoding='utf-8') # read utf8 stdin
+
+    if args.strs_replace_in:
+        rep_pairs = []
+        with open(args.strs_replace_in, 'r', encoding='utf-8') as f_rep_pairs:
+            for line in f_rep_pairs:
+                old, new = line.strip().split(args.strs_replace_sep)
+                rep_pairs.append({'old': old, 'new': new})
 
     if args.chars_delete:
         with open(args.chars_delete, 'r', encoding='utf-8') as df:
@@ -94,8 +108,14 @@ def main():
                 patterns.append(pattern)
 
     for line in f:
+        line = line.strip()
+        if args.str2lower: line = line.lower()
+        if args.strs_replace_in:
+            for pair in rep_pairs:
+                line = line.replace(pair['old'], pair['new'])
+
         # tokens = line.strip().split()
-        tokens = re.split("\s+", line.strip())
+        tokens = re.split("\s+", line)
         # Split the first skip_ncols tokens, take the remaining
         skipped_str = " ".join(tokens[:args.skip_ncols])
         remained_str = " ".join(tokens[args.skip_ncols:])
@@ -145,7 +165,9 @@ def main():
                         for i in range(0, len(chars), args.nchars_as_token)]
 
         # print(" ".join(char_strings))
-        sys.stdout.buffer.write((" ".join(char_strings)+'\n').encode('utf-8')) # print utf8 string
+        line = " ".join(char_strings)
+        if args.str2lower: line = line.lower()
+        sys.stdout.buffer.write((line +'\n').encode('utf-8')) # print utf8 string
 
 if __name__ == "__main__":
     main()
