5493ac5656d76c18cd6588b4632c59e8c7c34a69
bin-wu <wu.bin.vq9@is.naist.jp>
2020-04-28 22:28:50 +0900

    add save option to util.py

--- egs/wsj/s0/clib/comm/util.py
+++ egs/wsj/s0/clib/comm/util.py
@@ -1,3 +1,5 @@
+from typing import List, Dict, Tuple, Union, Any
+
 import sys
 import logging
 
@@ -19,3 +21,6 @@ def init_logger(file_name="", stream="stdout"):
         logger.addHandler(file_handler)
 
     return logger
+
+def save_options(options: Dict, path: str) -> None:
+    json.dump(options, open(path, 'w'), indent=4)
