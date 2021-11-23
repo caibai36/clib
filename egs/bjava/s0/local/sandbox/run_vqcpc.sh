a682061035b2af53e0fd5780009d79fda60d6751
bin-wu <wu.bin.vq9@is.naist.jp>
2021-11-23 17:09:04 +0900

    add code to run vqcpc

---
+++ egs/bjava/s0/local/sandbox/run_vqcpc.sh
@@ -0,0 +1,5 @@
+./local/sandbox/asr_general/run_vqcpc.sh --cat_vqcpc_asr true --vqcpc_epoch 5000 2>&1 | tee exp/logs/vqcpc5000.log
+./local/sandbox/asr_general/run_vqcpc.sh --cat_vqcpc_asr true --vqcpc_epoch 10000 2>&1 | tee exp/logs/vqcpc10000.log
+./local/sandbox/asr_general/run_vqcpc.sh --cat_vqcpc_asr true --vqcpc_epoch 15000 2>&1 | tee exp/logs/vqcpc15000.log
+./local/sandbox/asr_general/run_vqcpc.sh --cat_vqcpc_asr true --vqcpc_epoch 20000 2>&1 | tee exp/logs/vqcpc20000.log
+./local/sandbox/asr_general/run_vqcpc.sh --cat_vqcpc_asr true --vqcpc_epoch 22000 2>&1 | tee exp/logs/vqcpc22000.log
