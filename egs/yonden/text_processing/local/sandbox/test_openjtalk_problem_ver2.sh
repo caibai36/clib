84ceeaf60fd50905b5d504a930cee83d76ea667e
bin-wu <wu.bin.vq9@is.naist.jp>
2022-10-21 16:25:34 +0900

    update path for normalization scription for test openjtalk problem ver2

--- egs/yonden/text_processing/local/sandbox/test_openjtalk_problem_ver2.sh
+++ egs/yonden/text_processing/local/sandbox/test_openjtalk_problem_ver2.sh
@@ -17,4 +17,4 @@ echo "normalize the openjtalk text with the mecab text with dict..."
 cat data/text/openjtalk_text_problem.scp | python local/scripts/openjtalk.py --dict_dir /project/nakamura-lab08/Work/bin-wu/share/data/openjtalk_dict/openjtalk_default_unidic_ipadic --pos --has_uttid --user_dict data/dict/yonden_ver3_openjtalk.dic 2>/dev/null | tee exp/output/text.openjtalk
 cat data/text/openjtalk_text_problem.scp | python local/scripts/mecab.py --dict_dir /project/nakamura-lab08/Work/bin-wu/share/data/mecab_dict/unidic-csj-3.1.0-full --pos --has_uttid --user_dict data/dict/yonden_ver3_mecab_unidic_csj3.dic |& tee exp/output/text.mecab
 # python local/normalize_openjtalk_with_mecab.py --openjtalk_text exp/output/text.openjtalk --mecab_text exp/output/text.mecab --has_uttid --verbose |& tee exp/output/text.openjtalk.mecab
-python local/normalize_openjtalk_with_mecab_chouon_local.py --openjtalk_text exp/output/text.openjtalk --mecab_text exp/output/text.mecab --has_uttid --verbose
+python local/scripts/normalize_openjtalk_with_mecab_chouon_local.py --openjtalk_text exp/output/text.openjtalk --mecab_text exp/output/text.mecab --has_uttid --verbose
