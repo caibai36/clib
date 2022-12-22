ea938dc35c1d6c39941ac3b812f5f2d43c021d6a
bin-wu <wu.bin.vq9@is.naist.jp>
2022-12-22 23:13:56 +0900

    add runing scripts
    Former-commit-id: e22db18669475959d427638cb8f0891a183478af

---
+++ egs/timit/s1/local/sandbox/run_lm_word_beam.sh
@@ -0,0 +1,4 @@
+./run.sh --stage 7 --file_lm data/train/lm/train.word_6gram.arpa --type_lm "word_ngram" --coeff_lm 0.9 --beam_size 20 --exp_dir exp/tmp_lm_beam20
+./run.sh --stage 7 --file_lm data/train/lm/train.word_5gram.arpa --type_lm "word_ngram" --coeff_lm 0.9 --beam_size 20 --exp_dir exp/tmp_lm_beam20
+./run.sh --stage 7 --file_lm data/train/lm/train.word_4gram.arpa --type_lm "word_ngram" --coeff_lm 0.9 --beam_size 20 --exp_dir exp/tmp_lm_beam20
+./run.sh --stage 7 --file_lm data/train/lm/train.word_3gram.arpa --type_lm "word_ngram" --coeff_lm 0.9 --beam_size 20 --exp_dir exp/tmp_lm_beam20
