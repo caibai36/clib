9958e0e26fed0a143bae70e6c4df79bd234d1df1
bin-wu <bin.wu@riken.jp>
2023-07-17 12:14:53 +0900

    add run file for mit data
    Former-commit-id: 7230c9d91594a1a72bae7565d7c3f8a19e203a55

---
+++ egs/riken/mit_cnn_s0/local/sandbox/run_mit_data.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Set bash to 'debug' mode, it will exit on :
+# -e 'error', -u 'undefined variable', -o ... 'error in pipeline', -x 'print commands',
+set -uo pipefail
+
+# general configuration
+stage=0  # start from 0 if you need to start from data preparation
+
+# Data
+dataset_name=mit_data # see dataset shared by the paper of 'Close range vocal interaction in the common marmoset (Callithrix Jacchus)'
+mit_data=/data/share/bin-wu/data/marmoset/vocalization/marmoset_mit
+
+# Parse the options. (eg. ./run.sh --stage 1)
+# Note that the options should be defined as shell variable before parsing
+. local/scripts/parse_options.sh || exit 1
+
+if [ ${stage} -le 1 ]; then
+    date
+    echo "Data preparation..."
+    ./local/mit_data_data_prep.sh --mit_data $mit_data --stage 3
+    date
+fi
