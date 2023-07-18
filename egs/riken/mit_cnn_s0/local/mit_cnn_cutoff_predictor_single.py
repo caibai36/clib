e42f3a14f5daf4ef26e3f8b39a3c641107830953
bin-wu <bin.wu@riken.jp>
2023-07-18 13:13:56 +0900

    add cutoff to the predictions

---
+++ egs/riken/mit_cnn_s0/local/mit_cnn_cutoff_predictor_single.py
@@ -0,0 +1,73 @@
+'''creates text files out of predictions for single animal at a time (9 node output layer)'''
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import numpy as np
+import codecs
+
+truth_values=['cha','chi','ek','ph','ts','tr','trph','tw','noise']
+
+#returns the top prediction if has confidence higher than cutoff, otherwise returns noise
+def get_prediction(preds,cutoff):
+    max_pred=np.argmax(preds)
+    #if tr or ph, also count in confidence for trph
+    if max_pred in [3,5]:
+        if preds[max_pred]+preds[6]<cutoff:
+            return(8)
+        else:
+            return(max_pred)
+    
+    #if trph sum probs for tr, trph and ph
+    elif max_pred==6:
+        if preds[max_pred]+preds[3]+preds[5]<cutoff:
+            return(8)
+        else:
+            return(max_pred)
+    else:    
+        if preds[max_pred]<cutoff:
+            return(8)
+        else:
+            return(max_pred)
+ 
+def predict(pred_data,predictions_file1,cutoff):
+    fil1=open(pred_data,'rb')
+    predictions=np.load(fil1)
+    fil1.close()
+    #Uses utf-8 for Audacity compability
+    save_fil=codecs.open(predictions_file1,'w', 'utf-8')
+    for i in range(len(predictions)):
+        numb=get_prediction(predictions[i],cutoff)
+        if numb!=8:
+            save_fil.write('{:.3f}\t{:.3f}\t{}\n'.format((i)*0.05+0.325, (i+1)*0.05+0.325, truth_values[numb]))
+        
+    print('Done!')
+    save_fil.close()
+
+
+# if __name__=='__main__':
+#   for num in [0,0.3,0.4,0.5,0.6,0.65,0.7,0.75,0.8,0.85,0.9,0.95]:
+#       predict('Data/pred_20161219_Athos_model_small.npy',
+#               'results/20161219_Athos_model_small_{}%.txt'.format(int(100*num)),cutoff=num)
+#       predict('Data/pred_20161219_Porthos_model_small.npy',
+#               'results/20161219_Porthos_model_small_{}%.txt'.format(int(100*num)),cutoff=num)
+
+import os
+import argparse
+
+default_pred_files=["exp/sys/mit_sample/mit_sample0/mit_cnn_72-run0/bs25lr0.0003evalinterval200avgpredwin5/eval/test_pred1_Athos.npy", "exp/sys/mit_sample/mit_sample0/mit_cnn_72-run0/bs25lr0.0003evalinterval200avgpredwin5/eval/test_pred2_Porthos.npy"]
+parser = argparse.ArgumentParser(description="Cutoff the predictions (reference: 'cutoff_predictor_single.py' from https://marmosetbehavior.mit.edu/).")
+parser.add_argument("--cutoffs", type=float, default=[0.7, 0.8], nargs="+", help="values of cutoff. e.g., --cutoffs 0.7 0.8")
+parser.add_argument("--pred_files", type=str, default=default_pred_files, nargs="+", help='a sequence of predictions that need to be cut off')
+parser.add_argument("--out_dir", type=str, default="exp/sys/mit_sample/mit_sample0/mit_cnn_72-run0/bs25lr0.0003evalinterval200avgpredwin5/eval", help="output directory to store predictions after cut off")
+args = parser.parse_args()
+
+for cutoff in args.cutoffs:
+    for pred_file in args.pred_files:
+        file_name = os.path.splitext(os.path.basename(pred_file))[0]
+        cutoff_file_name = file_name + "_cutoff{}".format(cutoff) + ".txt"
+        cutoff_file = os.path.join(args.out_dir, cutoff_file_name)
+
+        print("Pred file: {}".format(pred_file))
+        print("Cutoff file: {}".format(cutoff_file))
+        predict(pred_file, cutoff_file, cutoff=cutoff)
--- egs/riken/mit_cnn_s0/local/sandbox/run_mit_data.sh
+++ egs/riken/mit_cnn_s0/local/sandbox/run_mit_data.sh
@@ -4,7 +4,7 @@
 set -uo pipefail
 
 # general configuration
-stage=3  # start from 0 if you need to start from data preparation
+stage=4  # start from 0 if you need to start from data preparation
 
 # Data and model options
 # model: mit_cnn_72
@@ -34,7 +34,7 @@ batch_size=25
 lr=0.0003
 # dropout=0.4 # for dropout layer
 # eps=0.001 # for adam optimizer
-cutoffs="0.7 0.8"
+cutoffs="0 0.3 0.4 0.5 0.6 0.65 0.7 0.75 0.8 0.85 0.9 0.95" # "0.7 0.8"
 num_iter=74001
 eval_interval=2000 # evaluate every x iterations and lr = lr * 0.97
 avg_pred_win=5 # collect predicted probabilities by averaging across x consecutive predictions with step size of 1
@@ -95,5 +95,14 @@ if [ ${stage} -le 3 ]; then
 	   --epsilon 0.001 \
 	   --avg_pred_win $avg_pred_win \
 	   --eval_model $result_dir/model.ckpt
+    date
+fi
+
+if [ ${stage} -le 4 ]; then
+    date
+    echo "cutoff mit cnn 72..."
+    python local/mit_cnn_cutoff_predictor_single.py --cutoffs $cutoffs \
+	   --pred_files $eval_dir/test_pred1_$first.npy $eval_dir/test_pred2_$sec.npy \
+	   --out_dir $eval_dir
     date
 fi
